3e1426b07f91ae77ff5f64475b7b60cdb7dcbbbe
Santtu Pajukanta <santtu@pajukanta.fi>
2016-12-31 01:02:04 +0200

    implement frontpage view

--- photoshoots/views/photoshoots_frontpage_view.py
+++ photoshoots/views/photoshoots_frontpage_view.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from ..models import Event
+
 
 def photoshoots_frontpage_view(request):
-    return render(request, 'photoshoots_frontpage_view.jade')
+    events = Event.objects.all()
+
+    vars = dict(
+        events=events,
+    )
+
+    return render(request, 'photoshoots_frontpage_view.jade', vars)
