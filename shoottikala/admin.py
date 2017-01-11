bcd961df7f4f4336ee6355ce115a839ae34a71a3
Santtu Pajukanta <santtu@pajukanta.fi>
2017-01-11 22:45:33 +0200

    such admin, much improve

--- shoottikala/admin.py
+++ shoottikala/admin.py
@@ -7,6 +7,16 @@ from .models import (
 )
 
 
-admin.site.register(Cosplayer)
+class CosplayerAdmin(admin.ModelAdmin):
+    list_display = ('event', 'display_name', 'character', 'source', 'source_type', 'is_active')
+    list_filter = ('event', 'is_active')
+
+
+class PhotographerAdmin(admin.ModelAdmin):
+    list_display = ('event', 'display_name', 'is_active')
+    list_filter = ('event', 'is_active')
+
+
+admin.site.register(Cosplayer, CosplayerAdmin)
 admin.site.register(Event)
-admin.site.register(Photographer)
+admin.site.register(Photographer, PhotographerAdmin)
