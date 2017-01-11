64656ba049b10d06c5483ae6d8e2e67ad7ba5931
Santtu Pajukanta <santtu@pajukanta.fi>
2017-01-11 23:01:29 +0200

    fix choice display in readonly forms

--- shoottikala/templatetags/shoottikala_tags.py
+++ shoottikala/templatetags/shoottikala_tags.py
@@ -5,12 +5,22 @@ from django import template
 register = template.Library()
 
 
+def get_value(form, field_name):
+    field = form.fields[field_name]
+    value = form.initial[field_name]
+
+    if getattr(field, 'choices', None):
+        return dict(field.choices).get(value, value)
+    else:
+        return value
+
+
 @register.inclusion_tag('shoottikala_readonly_form.jade')
 def readonly(form):
     '''
     Renders a quick & dirty read only version of a ModelForm.
     '''
 
-    fields = [(field, form.initial[field_name]) for (field_name, field) in form.fields.items()]
+    fields = [(field, get_value(form, field_name)) for (field_name, field) in form.fields.items()]
 
     return dict(fields=fields)
